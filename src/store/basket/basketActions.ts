064c458670d3ffa7bf364212ed902fdd06deed66
Hugo Fernandes <hugo.fernandes@magnumbank.com.br>
2024-07-07 14:37:50 -0300

    Make throttle on increment and decrement.

--- src/store/basket/basketActions.ts
+++ src/store/basket/basketActions.ts
@@ -1,25 +1,62 @@
-
 // Externals
 import { Action, Dispatch } from 'redux';
-import axios from 'axios'
+import axios from 'axios';
 
 import BasketItem from '../../models/BasketItem';
 import { IState } from '../rootReducer';
+
+let timerId: number = 0;
+
+function throttle(func: () => void, delay = 0) {
+  if (timerId) {
+    return;
+  }
+
+  timerId = setTimeout(function () {
+    func();
+
+    timerId = 0;
+  }, delay);
+}
+
+function calculateBasket() {
+  return (dispatch: Dispatch<Action>, getState: () => IState) => {
+    const state: IState = getState();
+
+    const newTotal = state.basket.items.reduce(
+      (previousValue: number, currentValue) => {
+        const basketItemTotal = currentValue.itemPrice * currentValue.quantity;
+
+        return previousValue + basketItemTotal;
+      },
+      0
+    );
+
+    // Simulate a real validate basket
+    axios
+      .get('https://2486713dae314753ae6b0ff127002d12.api.mockbin.io/')
+      .then(function () {
+        dispatch({
+          type: 'update-basket-totals',
+          payload: newTotal,
+        });
+      })
+      .finally(() => {
+        dispatch({
+          type: 'calculating_basket',
+          payload: false,
+        });
+      });
+  };
+}
 
 export function incrementItem(basketItem: BasketItem) {
   return (dispatch: Dispatch<Action>, getState: () => IState) => {
     const state: IState = getState();
 
-    // Call this same function after 3 seconds to avoid concurrency
-    if (state.basket.calculatingBasketInApi) {
-      return setTimeout(() => {
-        return incrementItem(basketItem)(dispatch, getState);
-      }, 3000);
-    }
-
     dispatch({
       type: 'calculating_basket',
-      payload: true
+      payload: true,
     });
 
     const basketItems = state.basket.items.map((item) => {
@@ -27,52 +64,23 @@ export function incrementItem(basketItem: BasketItem) {
         return {
           ...item,
           quantity: item.quantity + 1,
-        }
+        };
       }
-
       return item;
     });
 
-    const newTotal = basketItems.reduce((previousValue: number, currentValue) => {
-      const basketItemTotal = currentValue.itemPrice * currentValue.quantity;
-
-      return previousValue + basketItemTotal;
-    }, 0);
-
-
     dispatch({
       type: 'update-basket',
-      payload: basketItems
+      payload: basketItems,
     });
 
-    // Simulate a real validate basket
-    axios.get('https://2486713dae314753ae6b0ff127002d12.api.mockbin.io/')
-      .then(function () {
-        dispatch({
-          type: 'update-basket-totals',
-          payload: newTotal
-        });
-      })
-      .finally(() => {
-        dispatch({
-          type: 'calculating_basket',
-          payload: false
-        });
-      })
-
-  }
+    throttle(() => calculateBasket()(dispatch, getState), 3000);
+  };
 }
 
 export function decrementItem(basketItem: BasketItem) {
   return (dispatch: Dispatch<Action>, getState: () => IState) => {
     const state: IState = getState();
-
-    // Call this same function after 3 seconds to avoid concurrency
-    if (state.basket.calculatingBasketInApi) {
-      return setTimeout(() => {
-        return incrementItem(basketItem)(dispatch, getState);
-      }, 3000);
-    }
 
     const foundItem = state.basket.items.find((item) => {
       return item.id === basketItem.id;
@@ -85,7 +93,7 @@ export function decrementItem(basketItem: BasketItem) {
 
     dispatch({
       type: 'calculating_basket',
-      payload: true
+      payload: true,
     });
 
     const basketItems = state.basket.items.map((item) => {
@@ -93,37 +101,17 @@ export function decrementItem(basketItem: BasketItem) {
         return {
           ...item,
           quantity: item.quantity - 1,
-        }
+        };
       }
 
       return item;
     });
 
-    const newTotal = basketItems.reduce((previousValue: number, currentValue) => {
-      const basketItemTotal = currentValue.itemPrice * currentValue.quantity;
-
-      return previousValue + basketItemTotal;
-    }, 0);
-
     dispatch({
       type: 'update-basket',
-      payload: basketItems
+      payload: basketItems,
     });
 
-    // Simulate a real validate basket
-    axios.get('https://2486713dae314753ae6b0ff127002d12.api.mockbin.io/')
-      .then(function () {
-        dispatch({
-          type: 'update-basket-totals',
-          payload: newTotal
-        });
-      })
-      .finally(() => {
-        dispatch({
-          type: 'calculating_basket',
-          payload: false
-        });
-      })
-
-  }
+    throttle(() => calculateBasket()(dispatch, getState), 3000);
+  };
 }
